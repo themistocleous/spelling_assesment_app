a53a8699225d7564ed07df3d24bdc8b35f8f88f8
Haris Themistocleous <themistocleous@gmail.com>
2021-06-10 08:44:17 -0400

    update

--- spelling.py
+++ spelling.py
@@ -2,15 +2,17 @@ import os
 import re
 import math
 import string
-import sys, getopt
+import sys
+import getopt
 import numpy as np
 import pandas as pd
 from scipy import interp
 from collections import Counter
 from toipa import phonetic
+import warnings
 
-
-
+# To ignore code warnings at the output. Deactivate this if you modify the code.
+warnings.filterwarnings("ignore")
 
 
 def levenshtein(source, target):
@@ -28,7 +30,7 @@ def levenshtein(source, target):
                     and source[i - 2] == target[j - 1]:
                 trans_dist = mtable[i - 2][j - 2] + sub_trans_cost
                 mtable[i][j] = min(mtable[i][j], trans_dist)
-    distance= mtable[len(source)][len(target)]
+    distance = mtable[len(source)][len(target)]
     return float(distance) / max(len(source), len(target))
 
 
@@ -52,32 +54,30 @@ def spellingdistance(FILE):
     W = DF[DF.type == "word"]
     NW['spelling_score'] = NW.apply(rdlevenshteinphonetics, axis=1)
     W['spelling_score'] = W.apply(rdlevenshtein, axis=1)
-    newdf =  pd.concat([NW, W], axis=0)
+    newdf = pd.concat([NW, W], axis=0)
     return newdf
 
 
-
 def main(argv):
-   inputfile = ''
-   outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-   except getopt.GetoptError:
-      print('python spelling.py -i <inputfile> -o <outputfile>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print('python spelling.py -i <inputfile> -o <outputfile>')
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         FILE = arg
-         out = spellingdistance(FILE)
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
-         out.to_csv(outputfile)
-         print("Spelling run without errors")
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+    except getopt.GetoptError:
+        print('python spelling.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('python spelling.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            FILE = arg
+            out = spellingdistance(FILE)
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+            out.to_csv(outputfile)
+            print("Spelling run without errors")
 
-    
 
 if __name__ == "__main__":
     main(sys.argv[1:])
